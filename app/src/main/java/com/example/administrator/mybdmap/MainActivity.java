910ff486b52e0821437e87e5df7c748977786316
xiangzilin05 <738448954@qq.com>
2016-07-28 13:21:04 +0800

    MyBDmap

--- app/src/main/java/com/example/administrator/mybdmap/MainActivity.java
+++ app/src/main/java/com/example/administrator/mybdmap/MainActivity.java
@@ -2,51 +2,151 @@ package com.example.administrator.mybdmap;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
+import com.baidu.mapapi.model.LatLng;
+
 
 public class MainActivity extends AppCompatActivity {
-    // 百度地图控件
-    private MapView mMapView = null;
-    // 百度地图对象
-    private BaiduMap bdMap;
+    private MapView mapView;
+    private BaiduMap baiduMap;
+
+    //定位相关变量
+    private LocationClient locationClient;
+    private MyLocationListener myLocationListener;
+    //是否第一次打开
+    private boolean isFirstIn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         //
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        init();
+
+        initView();
+
+        //定位
+        initLocation();
+
     }
 
-    /**
-     * 初始化方法
-     */
-    private void init() {
-        mMapView = (MapView) findViewById(R.id.bmapview);
+    private void initLocation() {
+        locationClient = new LocationClient(this);
+        myLocationListener = new MyLocationListener();
+        locationClient.registerLocationListener(myLocationListener);
+
+        LocationClientOption option = new LocationClientOption();
+        option.setCoorType("bd09ll");
+        option.setIsNeedAddress(true);
+        option.setOpenGps(true);
+        option.setScanSpan(1000);
+    }
+
+    private void initView() {
+        mapView = (MapView) findViewById(R.id.id_bmapview);
+        //通过 MapView 获得 BaiduMap
+        baiduMap = mapView.getMap();
+        //设置地图放大比例
+        MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.zoomTo(15f);
+        baiduMap.setMapStatus(mapStatusUpdate);
+    }
+
+    private class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            MyLocationData data = new MyLocationData.Builder()
+                    .accuracy(bdLocation.getRadius())
+                    .latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLongitude())
+                    .build();
+
+//            MyLocationConfiguration configuration = new MyLocationConfiguration(LocationMode.NORMAL, );
+
+            if (isFirstIn) {
+                //经纬度
+                LatLng latLng = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+                MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newLatLng(latLng);
+                baiduMap.animateMapStatus(mapStatusUpdate);
+                isFirstIn = false;
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //开始定位
+        locationClient.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id_map_common:
+                baiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.id_map_site:
+                baiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.id_map_traffic:
+                if (baiduMap.isTrafficEnabled()) {
+                    baiduMap.setTrafficEnabled(false);
+                    item.setTitle("实时交通(on)");
+                } else {
+                    baiduMap.setTrafficEnabled(true);
+                    item.setTitle("实时交通(off)");
+                }
+                break;
+            default:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mMapView.onResume();
+        mapView.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mMapView.onPause();
+        mapView.onPause();
     }
 
     @Override
     protected void onDestroy() {
-        mMapView.onDestroy();
-        mMapView = null;
         super.onDestroy();
+        mapView.onDestroy();
     }
 }
